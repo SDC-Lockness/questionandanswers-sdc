0e93839853c032fdb862d599fc09c8e5992dbc2a
Oscar Carvente <oscar.carvente@gmail.com>
2023-07-13 17:46:19 -0700

    Clean up models file

--- server/models/qandas.js
+++ server/models/qandas.js
@@ -17,24 +17,9 @@ module.exports = {
   },
 
   create: function (params, callback) {
-    console.log(params);
-    var queryString = 'INSERT INTO movies (title, watched) VALUES (?, 0)'
-    db.connection.query(queryString, params, function(err, results) {
-      if (err) {
-        console.log(err);
-      } else {
-        callback(err,results);
-      }
-    });
+
   },
   update: function (params, callback) {
-    var queryString = 'UPDATE movies, (SELECT id FROM movies WHERE title = ? ) AS newmovie SET movies.watched = ? WHERE movies.id = newmovie.id';
-    db.connection.query(queryString, params, function(err, results) {
-      if (err) {
-        console.log(err);
-      } else {
-        callback(err,results);
-      }
-    });
+
   }
 };
