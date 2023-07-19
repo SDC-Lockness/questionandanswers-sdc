a43b9166c49c26b596e89fc3ccecb9724db84497
Oscar Carvente <oscar.carvente@gmail.com>
2023-07-18 20:56:01 -0700

    Renamed port

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const morgan = require('morgan');
 var router = require('./routes.js');
 var cors = require('cors');
 const app = express();
-const PORT =  3001;
+const PORT = process.env.PORT || 3001;
 app.use(cors());
 app.use(morgan('dev'));
 app.use(express.json());
